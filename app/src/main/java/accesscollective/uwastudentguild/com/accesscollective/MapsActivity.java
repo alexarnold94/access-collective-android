495b2281aa2089fa0be7270d1d92e213f0470345
diva-lonial <diva.lonial@gmail.com>
2018-01-21 18:07:18 +0800

    Map gets bounds information from firebase
    Uses bound information on map creation, need to change it so it gets
    info from list activity or get position button
    Need to integrate getting marker information from firebase

--- app/src/main/java/accesscollective/uwastudentguild/com/accesscollective/MapsActivity.java
+++ app/src/main/java/accesscollective/uwastudentguild/com/accesscollective/MapsActivity.java
@@ -2,6 +2,7 @@ package accesscollective.uwastudentguild.com.accesscollective;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,10 +11,16 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +32,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
+    public static class campusBounds {
+
+        public float bottomLeftLat;
+        public float bottomLeftLong;
+        public float topRightLat;
+        public float topRightLong;
+        public int zoom;
+        //float bottomLeftLat, float bottomLeftLong, float bottomRightLat, float bottomRightLong, int zoom
+        public campusBounds() {
+            // ...
+        }
+    }
 
     /**
      * Manipulates the map once available.
@@ -39,11 +58,30 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
 
         mMap = googleMap;
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        //Change this so gets campusBoundsToGet from list activity or get location functionality
+        String campusBoundsToGet = "UWA";
+        DatabaseReference ref = mDatabase.child("campusBounds").child(campusBoundsToGet);
 
-        // Create bounds for campus which can not be scrolled out of, and centre camera within bounds
-        LatLngBounds uwaCampusBounds = new LatLngBounds(
-                new LatLng(-31.992284,115.8082333), new LatLng(-31.973574,115.8232533));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(uwaCampusBounds.getCenter(), 15));
+        // Attach a listener to read the data at our posts reference
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                campusBounds boundsToUse = dataSnapshot.getValue(campusBounds.class);
+                Log.i("INFO", "Getting bounds info:  " + Float.toString(boundsToUse.bottomLeftLat));
+
+                // Define bounds for campus, centre camera within bounds, and prevent scrolling outside of them
+                LatLngBounds uwaCampusBounds = new LatLngBounds(
+                        new LatLng(boundsToUse.bottomLeftLat, boundsToUse.bottomLeftLong), new LatLng(boundsToUse.topRightLat, boundsToUse.topRightLong));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(uwaCampusBounds.getCenter(), boundsToUse.zoom));
+                mMap.setLatLngBoundsForCameraTarget(uwaCampusBounds);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.i("INFO","The read failed: " + databaseError.getCode());
+            }
+        });
 
         // Create marker
         LatLng uwaLandMark = new LatLng(-31.979908, 115.818039);
