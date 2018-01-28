bcb26bfa671f1086fa64d3949a7b119301b9d37c
christian <13sivwrchr@gmail.com>
2018-01-28 12:04:09 +0800

    Select Campus java code

--- app/src/main/java/accesscollective/uwastudentguild/com/accesscollective/SelectCampus.java
+++ app/src/main/java/accesscollective/uwastudentguild/com/accesscollective/SelectCampus.java
@@ -1,11 +1,19 @@
 package accesscollective.uwastudentguild.com.accesscollective;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+
+
 
 public class SelectCampus extends AppCompatActivity {
 
@@ -13,9 +21,34 @@ public class SelectCampus extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_campus);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-    }
+        setTitle("Select Campus");
 
+
+        ListView resultsListView= (ListView) findViewById(R.id.results_listview);
+
+        HashMap<String,String> UWA = new HashMap<>();
+        UWA.put("UWA Crawley", "35 Stirling Highway, Crawley WA 6009");
+        UWA.put("UWA Claremont", "Princess Rd & Bay Road, Claremont WA 6010");
+        UWA.put("UWA Albany", "35 Stirling Terrace, Albany WA 6330");
+
+        List<HashMap<String,String>> listItems = new ArrayList<>();
+        SimpleAdapter adapter= new SimpleAdapter(this, listItems, R.layout.list_item,
+                new String[] {"First Line", "Second Line"},
+                new int[] {R.id.text1, R.id.text2});
+
+        Iterator it= UWA.entrySet().iterator();
+        while(it.hasNext()) {
+            HashMap<String,String> resultsMap = new HashMap<>();
+            Map.Entry pair= (Map.Entry)it.next();
+            resultsMap.put("First Line", pair.getKey().toString());
+            resultsMap.put("Second Line", pair.getValue().toString());
+            listItems.add(resultsMap);
+        }
+
+        resultsListView.setAdapter(adapter);
+    }
 }
